b74fd5267d3572865679b1eda1355c4ffd5dc5df
Rostyslav Chuvurin <rostislav.chuvurin@gmail.com>
2026-02-13 15:29:03 +0200

    Static fix v2

--- Lumiere/settings.py
+++ Lumiere/settings.py
@@ -130,6 +130,9 @@ STATICFILES_DIRS = [
 ]
 
 STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
     "staticfiles": {
         "BACKEND": "django.contrib.staticfiles.storage.ManifestStaticFilesStorage",
     },
